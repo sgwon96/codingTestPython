ca3ff00165ff4fd18a7d89c11e0debb2d7f005fc
sgwon96 <zxcvb5434@likelion.org>
2021-04-10 18:43:06 +0900

    README 업데이트 + 42,43 수정

--- codeUp100/41~60/42.py
+++ codeUp100/41~60/42.py
@@ -1,4 +1,4 @@
 a=float(input())
-print( format(a, ".2f") )
+print("%.2f"%a)
 
 # [기초-값변환] 실수 1개 입력받아 소숫점이하 자리 변환하기(설명)
--- codeUp100/41~60/43.py
+++ codeUp100/41~60/43.py
@@ -1,4 +1,4 @@
 a,b = map(float,input().split(" "))
-print(format(a/b, ".3f"))
+print("%.2f"%(a/b))
 
 # [기초-산술연산] 실수 2개 입력받아 나눈 결과 계산하기(py)
